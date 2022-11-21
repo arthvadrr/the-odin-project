c505b6d80727f8b69c2ed2b4dab70339a48530c6
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2022-11-20 19:15:51 -0800

    working on tic tac toe madness

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,34 +1,53 @@
 const init = () => {
-  const container = document.getElementById('board-container');
-  const boardElements = [[],[],[]];
+  const $section_boardContainer = document.getElementById('board-container');
+  const $button_start = document.getElementById('start');
+  const $section_options = document.getElementById('options');
+  const $boardElements = [[],[],[]];
+  
   let playerSign = 1;
   let computerSign = 2;
   
   //options
   const winLength = 3;
-
+  
   const deleteBoard = () => document.getElementById('board').remove();
+  
+  const resetGame = e => {
+    $section_options.classList.remove('display-none');
+    e.target.remove()
+    deleteBoard();
 
+  }
+
+  const removeEventListeners = () => $boardElements.forEach(r => r.forEach(s => {
+    if (s) s.removeEventListener("click", onClick_Square);
+  }));
+  
   const createNewBoard = () => {
-    const board = document.createElement('div');
-    board.setAttribute('id', 'board');
+    const $div_board = document.createElement('div');
+    const $button_quit = document.createElement('button');
+    $div_board.setAttribute('id', 'board');
     
-    for (let a = 0; a < boardElements.length; a++) {
+    for (let a = 0; a < $boardElements.length; a++) {
       const row = document.createElement('div');
       row.setAttribute('id', `row-${a}`)
-      board.appendChild(row);
+      $div_board.appendChild(row);
 
       for (let b = 0; b < 3; b++) {
         const square = document.createElement('div');
         square.addEventListener("click", onClick_Square);
         square.setAttribute("data-id", "0");
         square.classList.add("square");
-        boardElements[a].push(square);
+        $boardElements[a].push(square);
         row.appendChild(square);
       }
     }
 
-    container.appendChild(board)
+    $button_quit.setAttribute("id", "quit");
+    $button_quit.innerText = "Quit";
+    $button_quit.addEventListener("click", resetGame);
+    $section_boardContainer.appendChild($div_board)
+    $section_boardContainer.appendChild($button_quit);
   }
 
   const hasWinner = () => {
@@ -53,7 +72,7 @@ const init = () => {
       const getDirs = () => {
         dirArr.forEach(f => {
           const pos = f([x, y]);
-          if (boardElements?.[pos[0]]?.[pos[1]]) {
+          if ($boardElements?.[pos[0]]?.[pos[1]]) {
             tryDir.push(f);
           }
         });
@@ -64,7 +83,7 @@ const init = () => {
         let currentPos = tryDir[0]([x, y]);
 
         for (let i = 0; i < winLength - 1; i++) {
-          const ele = boardElements?.[currentPos[0]]?.[currentPos[1]];
+          const ele = $boardElements?.[currentPos[0]]?.[currentPos[1]];
           if (!ele) continue;
           if (ele.getAttribute("data-id") === dataID) {
             count++;
@@ -80,9 +99,9 @@ const init = () => {
       return false;
     }
 
-    for (let a = 0; a < boardElements.length; a++) {
-      for (let b = 0; b < boardElements.length; b++) {
-        const dataID = boardElements[a][b].getAttribute('data-id');
+    for (let a = 0; a < $boardElements.length; a++) {
+      for (let b = 0; b < $boardElements.length; b++) {
+        const dataID = $boardElements[a][b].getAttribute('data-id');
 
         if (dataID !== '0' && isInline(a, b, dataID)) {
           winner = dataID;
@@ -93,9 +112,10 @@ const init = () => {
           case "1" : xAmount++; break;
           case "2" : oAmount++; break;
         }
-        //TODO Check if board is full
       }
     }
+
+    if (xAmount + oAmount === 9) winner = "0";
 
     return winner;
   }
@@ -107,29 +127,43 @@ const init = () => {
     const empties = [];
     let pos;
 
-    for (let a = 0; a < boardElements.length; a++) {
-      for (let b = 0; b < boardElements[a].length; b++ ) {
-        if (boardElements[a][b].getAttribute("data-id") === "0") empties.push([a, b]);
+    for (let a = 0; a < $boardElements.length; a++) {
+      for (let b = 0; b < $boardElements[a].length; b++ ) {
+        if ($boardElements[a][b].getAttribute("data-id") === "0") empties.push([a, b]);
       }
     }
 
-    const shuffledEmpties = empties
-    .map(pos => ({ pos, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ pos }) => pos);
-    pos = shuffledEmpties[0];
-    console.log(`pos:${pos}`);
-    console.log(shuffledEmpties);
-    setDataID(boardElements[pos[0]][pos[1]], computerSign);
+    if (empties.length > 0) {
+      const shuffledEmpties = empties
+      .map(pos => ({ pos, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ pos }) => pos);
+      pos = shuffledEmpties[0];
+      $boardElements[pos[0]][pos[1]].removeEventListener("click", onClick_Square);
+      setDataID($boardElements[pos[0]][pos[1]], computerSign);
+    }
   }
 
   const onClick_Square = (e) => {
-    setDataID(e.target, playerSign);
-    computerMove();
-    console.log(hasWinner());
+    e.target.removeEventListener("click", onClick_Square);
+      setDataID(e.target, playerSign);
+      computerMove();
+      
+      const winner = hasWinner();
+
+      if (winner) {
+        removeEventListeners();
+        //displayWinner();
+        //createResetButton();
+      }
   }
 
-  createNewBoard();
+
+  $button_start.addEventListener("click", e => {
+    e.target.classList.add("display-none");
+    $section_options.classList.add("display-none");
+    createNewBoard();
+  })
 }
 
 init();
